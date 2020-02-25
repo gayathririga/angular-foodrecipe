3dbe8b3d691dd4261ab75c9356125b5dbb932460
gayathririga <mailtogayathri05@gmail.com>
2020-02-25 16:34:22 +0530

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgbModule  ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    NgbModule  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    HeaderComponent, RecipesComponent, RecipeListComponent ],
+  bootstrap:    [ 
+    AppComponent ]
 })
 export class AppModule { }
